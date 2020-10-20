d953b73b12612ce7c1d7b64b3df625415c56a8ac
JuggerRUS <juggerrus@gmail.com>
2020-10-21 01:15:12 +0300

    initial update

---
+++ TF1.py
@@ -0,0 +1,9 @@
+# TensorFlow и tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Вспомогательные библиотеки
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
